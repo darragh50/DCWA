299fb79ef8f3067a7a79ac4ae1d8eed3a081b4c1
darragh50 <darraghrohan@gmail.com>
2024-12-17 16:00:45 +0000

    Complete routing + create Mongoose schema. Display MongoDB data

--- index.js
+++ index.js
@@ -2,14 +2,22 @@
 var express = require('express')
 let ejs = require('ejs');
 var app = express();
+const mongoose = require('mongoose');
 
 //Set view engine
 app.set('view engine', 'ejs')
 
-/*var employees = [{ id: "X103", name: "John Smith", salary: 45000 },
-    { id: "XT92", name: "Mary Murphy", salary: 41750 },
-    { id: "B10C", name: "Alan Collins", salary: 40000 },
-    { id: "YY12", name: "Brian Brogan", salary: 43250 } ]*/
+//Default mongoose connection url 
+mongoose.connect('mongodb://localhost:27017/proj2024MongoDB', {
+});
+
+//Define mongoose schema
+const lecturerSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    did: String
+});
+const Lecturer = mongoose.model('Lecturer', lecturerSchema);
 
 //Listening on port 3004
 app.listen(3004, () => {
@@ -32,4 +40,16 @@ app.get("/students", (req, res) => {
 //Route to grades page
 app.get("/grades", (req, res) => {
     res.send("<h1>Grades</h1>");
+});
+
+//Route to lecturers page (MongoDB)
+app.get("/lecturers", async (req, res) => {
+    try {
+        //Variable that holds all lecturers in proj2024MongoDB database
+        const lecturers = await Lecturer.find(); 
+        res.render("lecturers", { lecturers });
+    } 
+    catch (error) {
+        console.error("Error finding lecturers:", error);
+    }
 });
